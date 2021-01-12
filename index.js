f081ddb4151c4f94ba26f2d33c6bfa0861741d10
antrikshmisri <antrikshmisri@gmail.com>
2021-01-12 13:59:08 +0530

    feat: added home page

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.static(__dirname + '/public'));
+app.use('/styles', express.static(__dirname + '/public'));
 app.set('views' , path.join(__dirname , 'views'))
 app.set('view engine' , 'ejs')
 
