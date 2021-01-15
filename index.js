c3706e0a9f45ec1f24e686f6161957c432d76ced
antrikshmisri <antrikshmisri@gmail.com>
2021-01-15 13:48:35 +0530

    feat: added products page

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const mongoose = require('mongoose')
+const Product = require('./models/products')
+mongoose.connect('mongodb://localhost:27017/e-products',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+const db = mongoose.connection;
+db.on("error" , console.error.bind(console , "Connection error: "));
+db.once("open" , ()=>{
+    console.log("connection established")
+})
 
 app.use(express.static(__dirname + '/public'));
 app.use('/styles', express.static(__dirname + '/public'));
@@ -16,6 +29,11 @@ app.get('/signup' , (req , res)=>{
 })
 app.get('/signin' , (req , res)=>{
     res.render('e-commerce/signin')
+})
+
+app.get('/products' , async (req , res)=>{
+    const allproducts = await Product.find({})
+    res.render('e-commerce/products' , {allproducts})
 })
 
 const port = 5050
