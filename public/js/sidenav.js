d530ea3bba81b752d432e7a2de1e6f0c3e166962
antrikshmisri <antrikshmisri@gmail.com>
2021-01-16 23:58:57 +0530

    feat: added sidenav button animations

--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -1,10 +1,16 @@
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("mySidenav").style.transform = "translateX(0%)";
+      document.querySelector('.menu-btn').style.left = 'calc(100% - 1em - 50px - 1em)'
+      document.querySelector('.menu-btn').style.transform = 'rotateZ(540deg)'
   }
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.transform = "translateX(-100%)";
+    document.querySelector('.menu-btn').style.left = '0%'
+    document.querySelector('.menu-btn').style.transform = 'rotateZ(0deg)'
+
   }
 
   
