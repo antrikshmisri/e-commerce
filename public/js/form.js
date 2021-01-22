510ceac1b1d8d6d2b9af9369cc12527bfff83322
antrikshmisri <antrikshmisri@gmail.com>
2021-01-22 12:12:43 +0530

    fix: fixed validation logic

--- public/js/form.js
+++ public/js/form.js
@@ -1,57 +1,63 @@
-function validation(){
-  const email = document.getElementById('Email').value;
-
-  const password = document.getElementById('password').value;
-  const cpassword = document.getElementById('cpassword').value;
-
-if (email == "" ){
-  document.getElementById('useremail').innerHTML ="**Please enter the required field";
-  return false;
-}
-else{
-  document.getElementById('useremail').innerHTML ="";
-}
-if (password == "" ){
-  document.getElementById('userpassword').innerHTML ="**Please enter the required field";
-  return false;
-}
-else
+function validateEmail(elem)
 {
-  document.getElementById('userpassword').innerHTML ="";
+  if (elem.value == "" ){
+    document.getElementById('useremail').innerHTML ="**Please enter the required field";
+    return false;
+  }
+  else{
+    document.getElementById('useremail').innerHTML ="";
+  }
 }
-if(password.length<=7){
-  document.getElementById('userpassword').innerHTML ="**Password length should be greater than 7";
-  return false;
-}
-else{document.getElementById('userpassword').innerHTML ="";}
-if (password.search(/[0-9]/) == -1){
-  document.getElementById('userpassword').innerHTML ="**Password should contain atleast one numeric value";
-  return false;
-}
-else{document.getElementById('userpassword').innerHTML ="";}
 
-if (password.search(/[A-Z]/) == -1){
-  document.getElementById('userpassword').innerHTML ="**Password should contain atleast one upper case character";
-  return false;
+function validatePass(elem)
+{
+  if (elem.value == "" ){
+    document.getElementById('userpassword').innerHTML ="**Please enter the required field";
+    return false;
+  }
+  else
+  {
+    document.getElementById('userpassword').innerHTML ="";
+  }
+  if(elem.value.length<=7){
+    document.getElementById('userpassword').innerHTML ="**Password length should be greater than 7";
+    return false;
+  }
+  else{document.getElementById('userpassword').innerHTML ="";}
+  if (elem.value.search(/[0-9]/) == -1){
+    document.getElementById('userpassword').innerHTML ="**Password should contain atleast one numeric value";
+    return false;
+  }
+  else{document.getElementById('userpassword').innerHTML ="";}
+  
+  if (elem.value.search(/[A-Z]/) == -1){
+    document.getElementById('userpassword').innerHTML ="**Password should contain atleast one upper case character";
+    return false;
+  }
+  else{document.getElementById('userpassword').innerHTML ="";}
+  if (elem.value.search(/[a-z]/) == -1){
+    document.getElementById('userpassword').innerHTML ="**Password should contain atleast one lower case character";
+    return false;
+  }
+  else{document.getElementById('userpassword').innerHTML ="";}
+  if (elem.value.search(/[&,$,#,@]/) == -1){
+    document.getElementById('userpassword').innerHTML ="**Password should contain atleast one special case character";
+    return false;
+  
+  }
 }
-else{document.getElementById('userpassword').innerHTML ="";}
-if (password.search(/[a-z]/) == -1){
-  document.getElementById('userpassword').innerHTML ="**Password should contain atleast one lower case character";
-  return false;
-}
-else{document.getElementById('userpassword').innerHTML ="";}
-if (password.search(/[&,$,#,@]/) == -1){
-  document.getElementById('userpassword').innerHTML ="**Password should contain atleast one special case character";
-  return false;
 
-}
-else{document.getElementById('userpassword').innerHTML ="";}
-if (cpassword == "" ){
+
+function validatecPass(elem){
+
+const password = document.getElementById('password').value;
+
+if (elem.value == "" ){
   document.getElementById('confirmpass').innerHTML ="**Please enter the required field";
   return false;
 }
 else{document.getElementById('confirmpass').innerHTML ="";}
-if (password != cpassword){
+if (password != elem.value){
   document.getElementById('confirmpass').innerHTML ="**Passwords dosen't match";
   return false;
 }
