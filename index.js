612e0030fdd42ea67bb4993e8a79af2f1ee54562
antrikshmisri <antrikshmisri@gmail.com>
2021-02-01 15:17:12 +0530

    feat: added create route button

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const app = express()
 const mongoose = require('mongoose')
 const Product = require('./models/products')
 const bodyparser = require('body-parser')
-mongoose.connect('mongodb://localhost:27017/e-products',{
+mongoose.connect('mongodb://localhost:27017/e-products', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 })
 
 const db = mongoose.connection;
-db.on("error" , console.error.bind(console , "Connection error: "));
-db.once("open" , ()=>{
+db.on("error", console.error.bind(console, "Connection error: "));
+db.once("open", () => {
     console.log("connection established")
 })
 
@@ -23,90 +23,87 @@ app.use(express.static(__dirname + '/public'));
 app.use('/styles', express.static(__dirname + '/public'));
 app.use('/js', express.static(__dirname + '/public'));
 
-app.set('views' , path.join(__dirname , 'views'))
-app.set('view engine' , 'ejs')
-app.get('/home' , (req , res)=>{
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/home', (req, res) => {
     res.render('e-commerce/home')
 })
 
-app.get('/signup' ,  (req , res)=>{
+app.get('/signup', (req, res) => {
     res.render('e-commerce/signup')
 })
-app.get('/signin' , (req , res)=>{
+app.get('/signin', (req, res) => {
     res.render('e-commerce/signin')
 })
-app.get('/thank' , (req , res)=>{
+app.get('/thank', (req, res) => {
     res.render('e-commerce/thank')
 })
-app.get('/products' , async (req , res)=>{
+app.get('/products', async (req, res) => {
     const allproducts = await Product.find({})
-    const {category} = req.query
+    const { category } = req.query
     let categories = await Product.find({}).select('category -_id')
     let uniqecat = []
-    for(let category of categories)
-    {
+    for (let category of categories) {
         uniqecat.push(category.category)
     }
     uniqecat = [...new Set(uniqecat)]
     console.log(uniqecat)
-    if(category)
-    {
-        const filterproducts = await Product.find({category: category})
-        res.render('e-commerce/products/products' , {allproducts: filterproducts,uniqecat,currentcat: category})
+    if (category) {
+        const filterproducts = await Product.find({ category: category })
+        res.render('e-commerce/products/products', { allproducts: filterproducts, uniqecat, currentcat: category })
     }
-    else
-    {
+    else {
         const filterproducts = await Product.find({})
-        res.render('e-commerce/products/products' , {allproducts: filterproducts,uniqecat,currentcat: 'All'})
+        res.render('e-commerce/products/products', { allproducts: filterproducts, uniqecat, currentcat: 'All' })
     }
 
 })
 
-app.get('/products/new' , (req , res)=>{
+app.get('/products/new', (req, res) => {
     res.render('e-commerce/products/new')
 })
 
-app.post('/products' , async(req , res)=>{
+app.post('/products', async (req, res) => {
     const newproduct = new Product(req.body)
     await newproduct.save()
-    .then(res => {
-        console.log(res)
-    })
-    .catch(err =>{
-        console.log(err)
-    })
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
     res.redirect('/products')
 })
-app.get('/products/:id' , async (req , res)=>{
-    const {id} = req.params
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params
     const foundproduct = await Product.findById(id)
-    res.render('e-commerce/products/show' , {foundproduct})
+    res.render('e-commerce/products/show', { foundproduct })
 })
 
-app.get('/products/:id/edit' , async (req , res)=>{
-    const {id} = req.params
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
     const foundproduct = await Product.findById(id)
-    res.render('e-commerce/products/edit' , {foundproduct})
+    res.render('e-commerce/products/edit', { foundproduct })
 })
 
-app.put('/products/:id' , async (req , res)=>{
-    const {id} = req.params
-    const updatedproduct = await Product.findByIdAndUpdate(id , req.body , {runValidators: true})
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const updatedproduct = await Product.findByIdAndUpdate(id, req.body, { runValidators: true })
     res.redirect(`/products/${id}`)
 })
 
-app.get('/products/:id/delete' , async (req , res)=>{
-    const {id} = req.params
+app.get('/products/:id/delete', async (req, res) => {
+    const { id } = req.params
     const foundproduct = await Product.findById(id)
-    res.render("e-commerce/products/delete" , {foundproduct})
+    res.render("e-commerce/products/delete", { foundproduct })
 })
-app.delete('/products/:id' , async (req , res)=>{
-    const {id} = req.params
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params
     const product = await Product.findByIdAndDelete(id)
     res.redirect('/products')
 })
 
 const port = 5050
-app.listen(5050 , () => {
+app.listen(5050, () => {
     console.log(`Listening on Port No. ${port}`)
 })
